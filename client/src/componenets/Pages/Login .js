f4950f0c32dc8b3b7594bb9eea4790a2eb794ba4
surabhikanade <surabhikanade.com>
2021-02-03 13:23:34 +0530

    PARTY_APP uploaded

---
+++ client/src/componenets/Pages/Login .js
@@ -0,0 +1,55 @@
+import React, { useState ,useContext,useEffect} from "react";
+import AuthContext from '../../context/authContext/authContext'
+import { Link } from "react-router-dom";
+import Grid from '@material-ui/core/Grid';
+
+export const Login = (props) => {
+  const{loginUser,userAuth,errors,clearError} = useContext(AuthContext)
+  useEffect(()=>{
+    if(userAuth){
+ props.history.push('/')
+    }
+   },[userAuth,props.history])
+    const [ user, setUser ] = useState({
+      
+        email: "",
+        password: "",
+       
+      });
+      const {  email, password,} = user;
+    
+      const handleChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+        clearError()
+      };
+      const submit = (e) => {
+        e.preventDefault();      
+          loginUser({  email, password });
+          clearError()
+        
+      };
+  return (
+    <div className="login">
+     
+      
+        <h1>Login</h1>
+        <form onSubmit={submit}>
+          <input type="email" name="email" placeholder="email" value={email} onChange={handleChange} />
+          <input type="password" name="password" placeholder="Password" value={password} onChange={handleChange} />
+          <input type="submit" value="Sign In" className="btn" />
+        </form>
+        <div className="question">
+        {errors != null && <button className="danger">
+            {errors.msg ? errors.msg : errors.error[0].msg}
+            <span onClick={()=> clearError()}>X</span></button>}
+          <p>
+            {" "}
+            Don't have an account? {""} <Link to="/register">Sign Up</Link>
+          </p>
+        </div>
+        
+    </div>
+  );
+};
+
+export default Login;
